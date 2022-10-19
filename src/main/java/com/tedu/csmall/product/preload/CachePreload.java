7968881cc883c25a13e94ec55f8b4a4f53ecb352
meettry <1326508505@qq.com>
2022-10-19 10:38:22 +0800

    Initial commit

--- src/main/java/com/tedu/csmall/product/preload/CachePreload.java
+++ src/main/java/com/tedu/csmall/product/preload/CachePreload.java
@@ -16,10 +16,7 @@ import java.util.List;
 @Slf4j
 @Component
 public class CachePreload implements ApplicationRunner {
-    @Autowired
-    BrandRedisRepository brandRedisRepository;
-    @Autowired
-    BrandMapper brandMapper;
+
 
     public CachePreload() {
         log.debug("创建ApplicationRunner接口的实现对象:CachePreload");
@@ -29,20 +26,6 @@ public class CachePreload implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         log.debug("开始处理缓存预热");
 
-        log.debug("删除缓存中原有的品牌数据");
-        Long result = brandRedisRepository.deleteAll();
-        log.debug("共删除{}条数据",result);
 
-        log.debug("从mapper中读取品牌列表");
-        List<BrandListItemVO> list = brandMapper.list();
-
-        log.debug("将品牌列表写入到Redis");
-        brandRedisRepository.save(list);
-
-        log.debug("逐一根据id从MySQL中读取品牌详情,并写入到Redis");
-        for (BrandListItemVO item : list) {
-            BrandStandardVO brand = brandMapper.getStandardById(item.getId());
-            brandRedisRepository.save(brand);
-        }
     }
 }
